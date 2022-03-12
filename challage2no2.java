55d8e5f4366e133bcf7292958b7d3f861fe4077c
faustairsyad <89232214+faustairsyad@users.noreply.github.com>
2022-03-12 10:55:44 +0700

    Update challage2no2.java

--- challage2no2.java
+++ challage2no2.java
@@ -11,16 +11,16 @@ public class challange2 {
         if (jumlahkeluarga == 3) {
 
             System.out.print("masukkan masukkan nama ayah");
-            String nama_ayah = UserInput.nextLine();
+            String nama_ayah = UserInput.next();
 
             System.out.print("masukkan masukkan nama ibu =");
-            String nama_ibu = UserInput.nextLine();
+            String nama_ibu = UserInput.next();
 
             System.out.print("masukkan masukkan nama anak_ke1 =");
-            String anak_ke1 = UserInput.nextLine();
+            String anak_ke1 = UserInput.next();
 
 
-            System.out.println(nama_ayah);
+            System.out.print(nama_ayah);
             System.out.print("------");
             System.out.println(nama_ibu);
             System.out.println(" "+" "+" "+" "+" "+"|");
@@ -30,16 +30,16 @@ public class challange2 {
 
         if (jumlahkeluarga == 4) {
             System.out.print("masukkan masukkan nama ayah =");
-            String nama_ayah = UserInput.nextLine();
+            String nama_ayah = UserInput.next();
 
             System.out.print("masukkan masukkan nama ibu =");
-            String nama_ibu = UserInput.nextLine();
+            String nama_ibu = UserInput.next();
 
             System.out.print("masukkan masukkan nama anak_ke1 =");
-            String anak_ke1 = UserInput.nextLine();
+            String anak_ke1 = UserInput.next();
 
             System.out.print("masukkan masukkan nama anak_ke2 =");
-            String anak_ke2 = UserInput.nextLine();
+            String anak_ke2 = UserInput.next();
 
 
             System.out.println(nama_ayah + "-"+"-"+"-"+"-"+"-"+nama_ibu);
@@ -51,5 +51,255 @@ public class challange2 {
         }
 
 
+        if (jumlahkeluarga == 5) {
+            System.out.print("masukkan masukkan nama ayah =");
+            String nama_ayah = UserInput.next();
+
+            System.out.print("masukkan masukkan nama ibu =");
+            String nama_ibu = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke1 =");
+            String anak_ke1 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke2 =");
+            String anak_ke2 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke3 =");
+            String anak_ke3 = UserInput.next();
+
+
+            System.out.println(nama_ayah + "-"+"-"+"-"+"-"+"-"+nama_ibu);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke1);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke2);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke3);
+        }
+
+        if (jumlahkeluarga == 6) {
+            System.out.print("masukkan masukkan nama ayah =");
+            String nama_ayah = UserInput.next();
+
+            System.out.print("masukkan masukkan nama ibu =");
+            String nama_ibu = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke1 =");
+            String anak_ke1 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke2 =");
+            String anak_ke2 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke3 =");
+            String anak_ke3 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke4 =");
+            String anak_ke4 = UserInput.next();
+
+
+            System.out.println(nama_ayah + "-"+"-"+"-"+"-"+"-"+nama_ibu);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke1);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke2);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke3);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke4);
+
     }
+
+        if (jumlahkeluarga == 7) {
+            System.out.print("masukkan masukkan nama ayah =");
+            String nama_ayah = UserInput.next();
+
+            System.out.print("masukkan masukkan nama ibu =");
+            String nama_ibu = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke1 =");
+            String anak_ke1 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke2 =");
+            String anak_ke2 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke3 =");
+            String anak_ke3 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke4 =");
+            String anak_ke4 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke5 =");
+            String anak_ke5 = UserInput.next();
+
+
+            System.out.println(nama_ayah + "-"+"-"+"-"+"-"+"-"+nama_ibu);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke1);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke2);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke3);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke4);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke5);
+
+        }
+
+        if (jumlahkeluarga == 8) {
+            System.out.print("masukkan masukkan nama ayah =");
+            String nama_ayah = UserInput.next();
+
+            System.out.print("masukkan masukkan nama ibu =");
+            String nama_ibu = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke1 =");
+            String anak_ke1 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke2 =");
+            String anak_ke2 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke3 =");
+            String anak_ke3 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke4 =");
+            String anak_ke4 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke5 =");
+            String anak_ke5 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke6 =");
+            String anak_ke6 = UserInput.next();
+
+
+            System.out.println(nama_ayah + "-"+"-"+"-"+"-"+"-"+nama_ibu);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke1);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke2);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke3);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke4);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke5);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke6);
+
+        }
+
+        if (jumlahkeluarga == 9) {
+            System.out.print("masukkan masukkan nama ayah =");
+            String nama_ayah = UserInput.next();
+
+            System.out.print("masukkan masukkan nama ibu =");
+            String nama_ibu = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke1 =");
+            String anak_ke1 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke2 =");
+            String anak_ke2 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke3 =");
+            String anak_ke3 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke4 =");
+            String anak_ke4 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke5 =");
+            String anak_ke5 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke6 =");
+            String anak_ke6 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke7 =");
+            String anak_ke7 = UserInput.next();
+
+
+            System.out.println(nama_ayah + "-"+"-"+"-"+"-"+"-"+nama_ibu);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke1);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke2);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke3);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke4);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke5);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke6);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke7);
+
+        }
+
+        if (jumlahkeluarga == 10) {
+            System.out.print("masukkan masukkan nama ayah =");
+            String nama_ayah = UserInput.next();
+
+            System.out.print("masukkan masukkan nama ibu =");
+            String nama_ibu = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke1 =");
+            String anak_ke1 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke2 =");
+            String anak_ke2 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke3 =");
+            String anak_ke3 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke4 =");
+            String anak_ke4 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke5 =");
+            String anak_ke5 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke6 =");
+            String anak_ke6 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke7 =");
+            String anak_ke7 = UserInput.next();
+
+            System.out.print("masukkan masukkan nama anak_ke8=");
+            String anak_ke8=UserInput.next();
+
+
+            System.out.println(nama_ayah + "-"+"-"+"-"+"-"+"-"+nama_ibu);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke1);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke2);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke3);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke4);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke5);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke6);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke7);
+            System.out.println(" "+" "+" "+" "+" "+"|");
+            System.out.println(" "+" "+" "+" "+" "+anak_ke8);
+        }
+        
+        
+         if (jumlahkeluarga > 10) { 
+             
+              System.out.println("maaf jumlah keluarga anda terlalu banyak. sebaiknya anda membuat team bola");
+              System.out.println("ibuk = :( ");
+             
+         }
+        
+  }
 }
+
