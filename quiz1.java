0c45e5065bfcb579d58110f9a683f749da6c2d48
faustairsyad <89232214+faustairsyad@users.noreply.github.com>
2022-03-18 15:57:12 +0700

    Create quiz1.java

---
+++ quiz1.java
@@ -0,0 +1,40 @@
+public class challange2 {
+    public static void main(String[] args) {
+        int jamSekarang = 11;
+        if (jamSekarang > 4 & jamSekarang < 11){
+            System.out.println("halo, selamat pagi!");
+        } else if (jamSekarang > 11 & jamSekarang < 15) {
+            System.out.println("selamat siang");
+        } else {
+            System.out.println("semoga hari ini anda bahagia. :-)");
+        }
+            }
+        }
+
+
+
+public class challange2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <4; i++){
+            for (int j = 4; j > 0; j--){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+            }
+        }
+
+
+
+
+public class challange2 {
+    public static void main(String[] args) {
+
+        int jamSekarang = 11;
+        if (jamSekarang > 4 & jamSekarang < 11) {
+            System.out.println("halo, selamat pagi!");
+            System.out.println("pagi ini pagi yang cerah....");
+        }
+    }
+}
